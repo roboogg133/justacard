704586d4e2020d1265feb5f245635b05bbba65c9
roboogg133 <Luizfernandojoaquimdasilva@proton.me>
2025-06-03 21:54:13 -0300

    login system from veritas

---
+++ config/config.go
@@ -0,0 +1,108 @@
+package config
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/url"
+	"os"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/joho/godotenv"
+)
+
+var Dsn string
+var DB *pgxpool.Pool
+var Pass string
+
+type Claims struct {
+	Username  string `json:"username"`
+	TokenType string `json:"token_type"`
+	jwt.RegisteredClaims
+}
+
+func InitDB() {
+
+	godotenv.Load()
+
+	var host = os.Getenv("DB_HOST")
+	var port = os.Getenv("DB_PORT")
+	var user = os.Getenv("DB_USER")
+	var passwordnotgood = os.Getenv("DB_PASSWORD")
+	var dbname = os.Getenv("DB_NAME")
+
+	var password = url.QueryEscape(passwordnotgood)
+
+	var Dsn = fmt.Sprintf("postgres://%s:%s@%s:%s/%s", user, password, host, port, dbname)
+
+	dbpool, err := pgxpool.New(context.Background(), Dsn)
+	if err != nil {
+		log.Panic("Error connecting to DATABASE : ", err)
+	}
+
+	err = dbpool.Ping(context.Background())
+	if err != nil {
+		log.Panic("Database isn't reachable : ", err)
+	}
+
+	DB = dbpool
+
+}
+
+func GenerateJWTAccessToken(username string) (string, error) {
+
+	godotenv.Load()
+
+	var Secret = []byte(Pass)
+	expirationTime := time.Now().Add(15 * time.Minute)
+
+	claims := &Claims{
+		Username:  username,
+		TokenType: "access",
+		RegisteredClaims: jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(expirationTime),
+			IssuedAt:  jwt.NewNumericDate(time.Now()),
+		},
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	return token.SignedString(Secret)
+
+}
+
+func TokenAuthenticate(tokenString string) (*Claims, error) {
+	claims := &Claims{}
+
+	token, err := jwt.ParseWithClaims(tokenString, claims, func(token *jwt.Token) (interface{}, error) {
+		return []byte(Pass), nil
+	})
+
+	if err != nil || !token.Valid {
+		return nil, err
+	}
+
+	return claims, nil
+}
+
+func GenerateJWTRefreshToken(username string) (string, error) {
+
+	godotenv.Load()
+
+	var Secret = []byte(Pass)
+	expirationTime := time.Now().Add(168 * time.Hour)
+
+	claims := &Claims{
+		Username:  username,
+		TokenType: "refresh",
+		RegisteredClaims: jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(expirationTime),
+			IssuedAt:  jwt.NewNumericDate(time.Now()),
+		},
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	return token.SignedString(Secret)
+
+}
---
+++ main.go
@@ -0,0 +1,287 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net/http"
+	"strings"
+	"time"
+	"veritas/config"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+	"github.com/jackc/pgx/v5"
+	"golang.org/x/crypto/bcrypt"
+)
+
+type LoginRequest struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+func HashPassword(password string) (string, error) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+
+	return string(bytes), err
+}
+
+func Authenticate(user string, rawpass string) bool {
+	config.InitDB()
+
+	var password string
+
+	// Search for the username
+	err := config.DB.QueryRow(context.Background(),
+		"SELECT password FROM users WHERE username = $1", user).Scan(&password)
+
+	if err != nil {
+		if err == pgx.ErrNoRows {
+			return false
+		} else {
+			log.Panic(err)
+		}
+	}
+
+	err = bcrypt.CompareHashAndPassword([]byte(password), []byte(rawpass))
+	if err != nil {
+		return false
+	} else {
+		return true
+	}
+
+}
+
+func Register(username string, password string) error {
+	config.InitDB()
+
+	_, err := config.DB.Exec(context.Background(),
+		"INSERT INTO users (username, password) VALUES ($1, $2)", username, password)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func AuthAccess() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		tokenString, err := c.Cookie("AccessToken")
+
+		if err != nil {
+			c.Redirect(http.StatusTemporaryRedirect, "/service/refresh")
+			c.AbortWithStatus(http.StatusForbidden)
+			return
+		}
+
+		if tokenString == "" {
+			c.Redirect(http.StatusTemporaryRedirect, "/service/refresh")
+			c.AbortWithStatus(http.StatusForbidden)
+			return
+		}
+
+		claims, err := config.TokenAuthenticate(tokenString)
+		if claims == nil {
+			c.Redirect(http.StatusTemporaryRedirect, "/service/refresh")
+			c.AbortWithStatus(http.StatusForbidden)
+			return
+		}
+		if err != nil {
+			c.Redirect(http.StatusTemporaryRedirect, "/service/refresh")
+			c.AbortWithStatus(http.StatusForbidden)
+			return
+		}
+		if claims.TokenType != "access" {
+			c.AbortWithStatus(http.StatusForbidden)
+			return
+		}
+
+		c.Set("username", claims.Username)
+		c.Next()
+	}
+}
+
+func AuthRefresh() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		tokenString, err := c.Cookie("RefreshToken")
+
+		if err != nil {
+			c.AbortWithStatus(http.StatusForbidden)
+			return
+		}
+
+		claims, err := config.TokenAuthenticate(tokenString)
+		if claims == nil {
+			c.AbortWithStatus(http.StatusForbidden)
+			return
+		}
+		if err != nil {
+			c.AbortWithStatus(http.StatusForbidden)
+			return
+		}
+		if claims.TokenType != "refresh" {
+			c.AbortWithStatus(http.StatusForbidden)
+			return
+		}
+
+		c.Set("username", claims.Username)
+		c.Next()
+	}
+}
+
+func main() {
+
+	r := gin.Default()
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"https://servidordomal.fun", "*://31.97.20.160"},
+		AllowMethods:     []string{"GET", "POST", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: false,
+		MaxAge:           12 * time.Hour,
+	}))
+	r.POST("/service/login", func(c *gin.Context) {
+
+		var req LoginRequest
+
+		if err := c.ShouldBindJSON(&req); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"response": "badrequest"})
+			return
+		}
+
+		value := Authenticate(req.Username, req.Password)
+
+		if value == true {
+			token, err := config.GenerateJWTAccessToken(req.Username)
+			if err != nil {
+				c.JSON(http.StatusInternalServerError, gin.H{"response": "failed to generate tokens"})
+				return
+			}
+			refresh, err := config.GenerateJWTRefreshToken(req.Username)
+			if err != nil {
+				c.JSON(http.StatusInternalServerError, gin.H{"response": "failed to generate tokens"})
+				return
+			}
+
+			http.SetCookie(c.Writer, &http.Cookie{
+				Name:     "AccessToken",
+				Value:    token,
+				MaxAge:   900,
+				Path:     "/",
+				Domain:   "servidordomal.fun",
+				Secure:   true,
+				HttpOnly: true,
+				SameSite: http.SameSiteStrictMode,
+			})
+
+			http.SetCookie(c.Writer, &http.Cookie{
+				Name:     "RefreshToken",
+				Value:    refresh,
+				MaxAge:   345600,
+				Path:     "/",
+				Domain:   "servidordomal.fun",
+				Secure:   true,
+				HttpOnly: true,
+				SameSite: http.SameSiteStrictMode,
+			})
+
+			c.Status(http.StatusOK)
+			return
+		}
+		if value == false {
+			c.Status(http.StatusUnauthorized)
+			return
+		}
+
+	})
+
+	r.POST("/service/register", func(c *gin.Context) {
+
+		var req LoginRequest
+
+		if err := c.ShouldBindJSON(&req); err != nil {
+			c.Status(http.StatusBadRequest)
+			return
+		}
+
+		if strings.ContainsAny(req.Username, " ") || strings.ContainsAny(req.Password, " ") || req.Password == "" || req.Username == "" {
+			c.JSON(http.StatusBadRequest, gin.H{"response": "invalid username or password"})
+			return
+		}
+
+		password, _ := HashPassword(req.Password)
+
+		err := Register(req.Username, password)
+		if err != nil {
+			c.JSON(http.StatusConflict, gin.H{"response": "username alredy been taken"})
+			return
+		} else {
+			c.Status(http.StatusCreated)
+			return
+		}
+
+	})
+
+	r.GET("/service/validate", AuthAccess(), func(c *gin.Context) {
+
+		c.JSON(http.StatusOK, gin.H{"response": c.MustGet("username").(string)})
+	})
+
+	r.GET("/service/refresh", AuthRefresh(), func(c *gin.Context) {
+
+		username := c.MustGet("username").(string)
+
+		token, err := config.GenerateJWTAccessToken(username)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"response": "error generating token"})
+			return
+		}
+		refresh, err := config.GenerateJWTRefreshToken(username)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"response": "error generating refresh token"})
+			return
+		}
+
+		http.SetCookie(c.Writer, &http.Cookie{
+			Name:     "AccessToken",
+			Value:    token,
+			MaxAge:   900,
+			Path:     "/",
+			Domain:   "servidordomal.fun",
+			Secure:   true,
+			HttpOnly: true,
+			SameSite: http.SameSiteStrictMode,
+		})
+
+		http.SetCookie(c.Writer, &http.Cookie{
+			Name:     "RefreshToken",
+			Value:    refresh,
+			MaxAge:   345600,
+			Path:     "/",
+			Domain:   "servidordomal.fun",
+			Secure:   true,
+			HttpOnly: true,
+			SameSite: http.SameSiteStrictMode,
+		})
+		c.Redirect(http.StatusSeeOther, "/home")
+
+	})
+
+	r.GET("/home", AuthAccess(), func(c *gin.Context) {
+		r.LoadHTMLFiles("pages/home.html")
+
+		username := c.MustGet("username").(string)
+
+		c.HTML(http.StatusOK, "home.html", gin.H{"Name": username})
+	})
+
+	r.GET("/service/createRoom", AuthAccess(), func(c *gin.Context) {
+		r.LoadHTMLFiles("pages/home.html")
+
+		username := c.MustGet("username").(string)
+
+		c.HTML(http.StatusOK, "home.html", gin.H{"Name": username})
+	})
+
+	r.Run(":8080")
+
+}
